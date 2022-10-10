981770ff4f89552cdd1dd577e412690580f6e708
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-10 09:19:40 -0500

    Initial Commit

---
+++ FormattingDemo/FormattingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormattingDemoApp.swift
+//  FormattingDemo
+//
+//  Created by Mark Volkmann on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FormattingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
